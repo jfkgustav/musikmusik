ba1341bb9f1b26bb67daf9faa782f8e61b2c8824
root <root@Gustavs-Brbara>
2024-04-11 10:51:03 +0200

    init commitit

---
+++ src/routes/musik/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { onMount } from 'svelte';
+	let music_table = [];
+
+	onMount(async () => {
+		const response = await fetch('https://localhost:7114/Song/');
+		const data = await response.json();
+		music_table = data;
+    console.log(data);
+  });
+
+
+</script>
+
+<svelte:head>
+	<title>Musik API</title>
+	<meta name="description" content="Musik API" />
+</svelte:head>
+
+<section class="container">
+	<div class="grid grid-cols-2 justify-between">
+		<p>id</p> <p>song name</p>
+		{#each music_table as song}
+   
+			<p>{song.id}</p>
+      <p>{song.name}</p>
+	 
+  {/each}
+	</div>
+</section>
